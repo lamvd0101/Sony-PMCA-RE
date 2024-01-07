25f7583350099516c86edddae7a6a55ac8eeb0d1
Liam <lamvd0101@gmail.com>
2024-01-07 10:34:07 +0700

    chore: sync

--- pmca/platform/tweaks.py
+++ pmca/platform/tweaks.py
@@ -128,8 +128,11 @@ class LanguageTweak(BackupTweak):
   return self._getLangs(region[region.index('_')+1:])
 
  def onValue(self):
-  # return self._getLangs('ALLLANG')
-  return self._getLangs('E32')
+  region = self._backup.getRegion()
+  if region[region.index('_')+1:] == 'J1':
+    return self._getLangs('JP-MOD')
+  else:
+    return self._getLangs('ALLLANG')
 
  def strValue(self):
   val = bytearray(self.read())
